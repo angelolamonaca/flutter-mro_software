447e08aec6ed773bde153b4d22fcc4f64ea45234
Angelo Lamonaca <angelo.lamonaca@gmail.com>
2022-02-13 14:19:48 +0100

    Set a fixed list and change app title

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,28 +14,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Aircraft Inspection Checklist',
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-      )),
-      home: const RandomWords(),
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+          )),
+      home: const InspectionItems(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class InspectionItems extends StatefulWidget {
+  const InspectionItems({Key? key}) : super(key: key);
 
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _InspectionItemsState createState() => _InspectionItemsState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
+class _InspectionItemsState extends State<InspectionItems> {
+  final _done = <String>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _inspectionList = [
+    "Oil Change",
+    "Equipment List",
+    "ELT Battery",
+    "Altimeter System",
+    "Pitot",
+    "Transponder",
+    "VOD Operational Check",
+    "Airworthness Certificate",
+    "Carbon Monoxide Detector",
+    "Survival Kit",
+    "Serviceable Fire Extinguisher",
+    "Tires",
+    "Anti Collision Strobe",
+    "Cabin"
+  ];
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -47,6 +61,7 @@ class _RandomWordsState extends State<RandomWords> {
         * and places each suggestion into a ListTile row.
         * Note that the divider might be difficult to see on smaller devices.
         */
+        itemCount: _inspectionList.length*2,
         itemBuilder: (context, i) {
           /*
           * For odd rows, a one-pixel-high divider widget before each row in the ListView,
@@ -64,34 +79,28 @@ class _RandomWordsState extends State<RandomWords> {
           */
           final index = i ~/ 2;
 
-          /*
-          * If you’ve reached the end of the available word pairings,
-          * then generate 10 more and add them to the suggestions list.
-          */
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-
           /* For even rows, the function adds a ListTile row for the word pairing. */
-          return _buildRow(_suggestions[index]);
+          return _buildRow(_inspectionList[index]);
         });
   }
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
+  Widget _buildRow(String inspectionItem) {
+    final alreadySaved = _done.contains(inspectionItem);
     return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
+      title: Text(inspectionItem, style: _biggerFont),
       trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+        alreadySaved
+            ? Icons.check_box_outlined
+            : Icons.check_box_outline_blank_outlined,
+        color: alreadySaved ? Colors.green : Colors.red,
+        semanticLabel: alreadySaved ? 'Remove from done' : 'Save',
       ),
       onTap: () {
         setState(() {
           if (alreadySaved) {
-            _saved.remove(pair);
+            _done.remove(inspectionItem);
           } else {
-            _saved.add(pair);
+            _done.add(inspectionItem);
           }
         });
       },
@@ -102,12 +111,12 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
+        title: const Text('Aircraft Inspection Checklist'),
         actions: [
           IconButton(
             icon: const Icon(Icons.list),
             onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
+            tooltip: 'Done Inspections',
           ),
         ],
       ),
@@ -119,11 +128,11 @@ class _RandomWordsState extends State<RandomWords> {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
         builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
+          final tiles = _done.map(
+                (inspectionItem) {
               return ListTile(
                 title: Text(
-                  pair.asPascalCase,
+                  inspectionItem,
                   style: _biggerFont,
                 ),
               );
@@ -131,14 +140,14 @@ class _RandomWordsState extends State<RandomWords> {
           );
           final divided = tiles.isNotEmpty
               ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
+            context: context,
+            tiles: tiles,
+          ).toList()
               : <Widget>[];
 
           return Scaffold(
             appBar: AppBar(
-              title: const Text('Saved Suggestions'),
+              title: const Text('Done Inspections'),
             ),
             body: ListView(children: divided),
           );
